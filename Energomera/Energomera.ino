2ff41bbc7537ca7f2e9800ee67c0ef8039d672a4
vgamaev <gamaev.va@gmail.com>
2020-02-12 09:38:37 +0300

    Update Energomera.ino

--- Energomera/Energomera.ino
+++ Energomera/Energomera.ino
@@ -1,38 +1,29 @@
-
 #include <SoftwareSerial.h>
+
 int i = 0;
 long Previous = 0; 
-int val = 0;
-int Time_1 = 1;         
-int Time_2 = 2;          
-int Time_3 = 3;
-int Time_4 = 4;
-int Time_5 = 5;
-int Time_6 = 6;
-int Time_7 = 7;
-int Time_8 = 8;
-int Time_9 = 15; 
+int Step = 0;
 
 char x [50];
 // открываем сессию
-byte cmd1[] = {0xaF,0x3F,0x21,0x8D,0x0A};
+byte CmdOpenSesion[] = {0xaF,0x3F,0x21,0x8D,0x0A};
 // читаем тип счетчика
-byte cmd2[] = {0x06,0x30,0x35,0xb1,0x8d,0x0a};
+byte CmdReadType[] = {0x06,0x30,0x35,0xb1,0x8d,0x0a};
 // снимаем показания
-byte cmd3[] = {0x81,0xd2,0xb1,0x82,0xc5,0xd4,0x30,0x50,0xc5,0x28,0xa9,0x03,0xb7};
+byte CmdEtope[] = {0x81,0xd2,0xb1,0x82,0xc5,0xd4,0x30,0x50,0xc5,0x28,0xa9,0x03,0xb7};
 //напряжение на фазах
-byte cmd4[] = {0x81,0xd2,0xb1,0x82,0x56,0xcf,0xcc,0xd4,0x41,0x28,0xa9,0x03,0x5f};
+byte CmdVolta[] = {0x81,0xd2,0xb1,0x82,0x56,0xcf,0xcc,0xd4,0x41,0x28,0xa9,0x03,0x5f};
 // мощность
-byte cmd5[] = {0x81,0xd2,0xb1,0x82,0x50,0xcf,0xd7,0xc5,0x50,0x28,0xa9,0x03,0xe4};
+byte CmdPower[] = {0x81,0xd2,0xb1,0x82,0x50,0xcf,0xd7,0xc5,0x50,0x28,0xa9,0x03,0xe4};
 // частота  
-byte cmd6[] = {0x81,0xd2,0xb1,0x82,0xc6,0xd2,0xc5,0xd1,0x55,0x28,0xa9,0x03,0x5c};
+byte CmdFrequ[] = {0x81,0xd2,0xb1,0x82,0xc6,0xd2,0xc5,0xd1,0x55,0x28,0xa9,0x03,0x5c};
 // ток  
-byte cmd7[] = {0x81,0xd2,0xb1,0x82,0xc3,0x55,0xd2,0xd2,0xc5,0x28,0xa9,0x03,0x5a};
+byte CmdCurre[] = {0x81,0xd2,0xb1,0x82,0xc3,0x55,0xd2,0xd2,0xc5,0x28,0xa9,0x03,0x5a};
 // cos f  
-byte cmd8[] = {0x81,0xd2,0xb1,0x82,0xc3,0x55,0xd2,0xd2,0xc5,0x28,0xa9,0x03,0x5a};
+byte CmdCos_f[] = {0x81,0xd2,0xb1,0x82,0xc3,0x55,0xd2,0xd2,0xc5,0x28,0xa9,0x03,0x5a};
 
 
-
+#define CYCLE_TIME 15
 #define DIR 8 
  
 SoftwareSerial RS485 (10, 11); // RX, TX
@@ -92,7 +83,7 @@ void PrintVolume(Energomera Buffer)
 
 void EnergomeraRead()
 {
-  switch (val) {
+  switch (Step) {
         case 1:
               
         break;
@@ -132,61 +123,65 @@ void EnergomeraRead()
 }
 
 
+void SendCommand(byte* cmd, int size)
+{
+    digitalWrite(DIR, HIGH);
+    RS485.write (cmd,size);
+    digitalWrite(DIR, LOW);
+}
+
 void EmergomeraWrite()
 {
-     if (val == Time_1)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd1,5);
-         digitalWrite(DIR, LOW);
-       }
-       if (val == Time_2)
-       {
-        digitalWrite(DIR, HIGH);
-         RS485.write (cmd2,6);
-        digitalWrite(DIR, LOW); 
-       }
-       if (val == Time_3)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd3,13);
-         digitalWrite(DIR, LOW);
-       }
-       if (val == Time_4)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd4,13);
-         digitalWrite(DIR, LOW);
-       }
-       if (val == Time_5)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd5,13);
-         digitalWrite(DIR, LOW);
-       }
-       if (val == Time_6)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd6,13);
-         digitalWrite(DIR, LOW);
-       }
-       if (val == Time_7)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd7,13);
-         digitalWrite(DIR, LOW);
-       }
-       if (val == Time_8)
-       {
-         digitalWrite(DIR, HIGH);
-         RS485.write (cmd8,13);
-         digitalWrite(DIR, LOW);
-       }
-       
-       if (val == Time_9)
-       {
-          val = 0;
-       }
+    switch (Step) {
+        case 1:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdOpenSesion,5);
+              digitalWrite(DIR, LOW);
+              SendCommand(CmdOpenSesion,5);
+        break;
+        case 2:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdReadType,6);
+              digitalWrite(DIR, LOW); 
+        break;
+        case 3:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdEtope,13);
+              digitalWrite(DIR, LOW);
+        break;
+        case 4:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdVolta,13);
+              digitalWrite(DIR, LOW);
+        break;
+        case 5:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdPower,13);
+              digitalWrite(DIR, LOW);
+        break;
+        case 6:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdFrequ,13);
+              digitalWrite(DIR, LOW);
+        break;
+        case 7:
+              digitalWrite(DIR, HIGH);
+              RS485.write (CmdCurre,13);
+              digitalWrite(DIR, LOW);
+        break;
+        case 8:
+               digitalWrite(DIR, HIGH);
+               RS485.write (CmdCos_f,13);
+               digitalWrite(DIR, LOW);
+        break;
+        case CYCLE_TIME:
+               Step = 0;
+        break;
+          default:
+  
+        break;
+      }
+     
 }
 
 void setup()
@@ -210,13 +205,13 @@ void loop()
       ReadStr += inChar;
     }
     
-    //ReadStr = "FREQU(50.00)";
+    ReadStr = "FREQU(50.00)";
       
     if (millis() - Previous > 1000)
     {
      Previous = millis();      
      EnergomeraRead();
-     val ++;
+     Step ++;
      EmergomeraWrite();          
      }
 }
